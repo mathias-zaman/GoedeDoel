f62100a8355334aa25319a7fc451cc211f8d51dd
Mathias <mathias.zaman@vdabcampus.be>
2026-02-17 12:10:53 +0100

    added tests for equals

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -23,4 +23,14 @@ public class GoedeDoelTest {
     void eenNieuwDoelHeeftNogGeenOpbrengst() {
         assertThat(doel.getOpbrengst()).isZero();
     }
+
+    @Test
+    void doelenMetDezelfdeNaamZijnGelijk() {
+        assertThat(doel).isEqualTo(new GoedeDoel(NAAM));
+    }
+
+    @Test
+    void doelenMetVerschillendeNaamZijnVerschillend() {
+        assertThat(doel).isNotEqualTo(new GoedeDoel("WWF"));
+    }
 }
